0978a59bcc5c171146873251e771ddbf9effb967
James Hartman <hartman.james.p@gmail.com>
2018-02-04 15:00:01 -0500

    added comic controller tests and factorybot

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -8,3 +8,13 @@ FactoryBot.define do
   end
 
 end
+
+FactoryBot.define do
+  factory :comic do
+    title "Watchmen"
+    author "Alan Moore"
+    description "(-:"
+    published_year 1987
+    file { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'test-file.pdf'), 'application/pdf') }
+  end
+end
