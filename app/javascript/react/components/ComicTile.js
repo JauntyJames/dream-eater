9ab41c4c6817edf9a588a962196458c4bda03cb3
James Hartman <hartman.james.p@gmail.com>
2018-02-07 17:14:09 -0500

    a little styling

--- app/javascript/react/components/ComicTile.js
+++ app/javascript/react/components/ComicTile.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 
 const ComicTile = (props) => {
   return(
-    <div className="callout large-3 shadow-drop-br">
+    <div className="callout primary large-3 shadow-drop-br">
       <Link to={`/comics/${props.id}`}>
       <img src={props.thumb} /><br/>
       {props.title}</Link>
