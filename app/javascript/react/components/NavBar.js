fbf3e395aae663bcf00e04b9c4f68b915d581d50
James Hartman <hartman.james.p@gmail.com>
2018-02-07 08:36:12 -0500

    created bookmark model and autoload

--- app/javascript/react/components/NavBar.js
+++ app/javascript/react/components/NavBar.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, browserHistory } from 'react-router'
 
-const NavBar = (props) => {
-  let newComic;
-  if (!props.route.signedIn){
-    newComic = <Link to='/sign-in'><button>Sign In</button></Link>
-  } else {
-    newComic = <Link to='comics/new'><button>Add Comic</button></Link>
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signedIn: false
+    }
   }
-  return(
-    <div>
-      <Link to='/'><button>Index</button></Link>
-      <button onClick={browserHistory.goBack}>Back</button>
-      {newComic}
-      { props.children }
-    </div>
-  )
+
+  componentWillMount() {
+    fetch('/auth/is_signed_in', {credentials: 'same-origin'})
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+            error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      this.setState({ signedIn: body.signed_in})
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
+  render() {
+    let newComic;
+    if (this.state.signedIn){
+      newComic = <Link to='comics/new'><button>Add Comic</button></Link>
+    } else {
+      newComic = <Link href='/users/sign_in'><button>Sign In</button></Link>
+    }
+    return(
+      <div>
+        <Link to='/'><button>Index</button></Link>
+        <button onClick={browserHistory.goBack}>Back</button>
+        {newComic}
+        { this.props.children }
+      </div>
+    )
+  }
 }
 export default NavBar;
