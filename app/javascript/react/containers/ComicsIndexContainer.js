53af00528695c3be94530de69f6529e6e03f55c6
James Hartman <hartman.james.p@gmail.com>
2018-02-08 11:44:40 -0500

    partly implemented fuzzy search bar

--- app/javascript/react/containers/ComicsIndexContainer.js
+++ app/javascript/react/containers/ComicsIndexContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FuzzySearch from 'react-fuzzy'
 
 import ComicTile from '../components/ComicTile'
 
@@ -6,10 +7,12 @@ class ComicsIndexContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comics: []
+      comics: [],
+      selected: []
     }
     this.componentDidMount = this.componentDidMount.bind(this)
   }
+
   componentDidMount() {
     fetch('api/v1/comics',
         {credentials: 'same-origin'}
@@ -25,27 +28,48 @@ class ComicsIndexContainer extends Component {
     })
     .then(response => response.json())
     .then(body => {
-
       this.setState({ comics: body.comics })
     })
     .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
-  render() {
-    let comicsArray = this.state.comics.map((comic) => {
-      return(
+  selectComics(selected) {
+    let comicsArray = selected.map(comic => {
+      return (
         <ComicTile
-          key={comic.id}
-          id={comic.id}
           title={comic.title}
+          author={comic.author}
           thumb={comic.thumbnail}
         />
       )
     })
+    return(comicsArray)
+  }
+
+  render() {
+    const list =
+      [{
+        id: 1,
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald'
+      }, {
+        id: 2,
+        title: 'The DaVinci Code',
+        author: 'Dan Brown'
+      }, {
+        id: 3,
+        title: 'Angels & Demons',
+        author: 'Dan Brown'
+      }];
 
     return(
       <div>
-        {comicsArray}
+        <FuzzySearch
+          list={list}
+          keys={['author', 'title']}
+          width={430}
+          onSelect={action('selected')}
+        />
       </div>
     )
   }
