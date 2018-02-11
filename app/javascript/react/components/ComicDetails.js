c587a33319458abad464d0959b42698f9a18dbdb
James Hartman <hartman.james.p@gmail.com>
2018-02-11 18:38:04 -0500

    added og tags for fb share

--- app/javascript/react/components/ComicDetails.js
+++ app/javascript/react/components/ComicDetails.js
@@ -6,6 +6,7 @@ const ComicDetails = (props) => {
   let publishedYear
   let description
   let id
+  let thumbnail
 
   if (props.comic !== null){
     title = props.comic.title
@@ -13,10 +14,18 @@ const ComicDetails = (props) => {
     publishedYear = props.comic.published_year
     description = props.comic.description
     id = props.comic.id
+    thumbnail = props.comic.thumbnail
   }
+
+  debugger
 
   return(
     <div>
+      <meta property="og:url"                content={`http://dream-eater.herokuapp/comics/${id}`} />
+      <meta property="og:type"               content="media" />
+      <meta property="og:title"              content={title} />
+      <meta property="og:description"        content={description} />
+      <meta property="og:image"              content={thumbnail} />
       <div className="fb-like" data-href={`http://dream-eater.herokuapp/comics/${id}`} data-width="75" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div>
       <h1>{title}</h1>
       <h3>By {author}</h3>
