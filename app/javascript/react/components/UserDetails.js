ca43893b50ccc4b2a313f6691981518c103d9909
James Hartman <hartman.james.p@gmail.com>
2018-02-12 11:15:48 -0500

    added user details to bookshelf page

--- app/javascript/react/components/UserDetails.js
+++ app/javascript/react/components/UserDetails.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 const UserDetails = (props) => {
+  let email
+  let profile_photo
+  let fullDate
+  const MonthNames = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+
+  if (props.user) {
+    email = props.user.email
+    profile_photo = props.user.profile_photo.url
+    let created = new Date(props.user.created_at)
+    let month = MonthNames[created.getMonth()]
+    fullDate = month + " " + created.getDate() + ", " + created.getFullYear()
+  }
 
   return(
-    <div></div>
+    <div>
+      <h1>User Details</h1>
+        <img src={profile_photo} width="100" height="100" className="avatar"></img>
+        <p>Email: {email}</p>
+        <p>User since: {fullDate}</p>
+    </div>
   )
 }
 export default UserDetails;
