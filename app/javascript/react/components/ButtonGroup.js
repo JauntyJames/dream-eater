74dd712ad231544ec3b47dc15296a32f07245be5
James Hartman <hartman.james.p@gmail.com>
2018-02-12 19:11:12 -0500

    pretty comic nav buttons

--- app/javascript/react/components/ButtonGroup.js
+++ app/javascript/react/components/ButtonGroup.js
@@ -4,12 +4,12 @@ const ButtonGroup = (props) => {
 
   return(
     <div className="comic-nav">
-      <button onClick={props.goToBegining}><i className="fas fa-angle-double-left"></i></button>
-      <button onClick={props.goFull}><i className="fas fa-expand"></i></button>
-      <button onClick={props.bookmarkPage}><i className="far fa-bookmark"></i></button>
-      <button onClick={props.addFavorite}><i className="far fa-heart"></i></button>
-      <button onClick={props.turnPageBack}><i className="fas fa-angle-left"></i></button>
-      <button onClick={props.turnPageForward}><i className="fas fa-angle-right"></i></button>
+      <button className="button round" onClick={props.goToBegining}><i className="fas fa-angle-double-left"></i></button>
+      <button className="button round" onClick={props.goFull}><i className="fas fa-expand"></i></button>
+      <button className="button round" onClick={props.bookmarkPage}><i className="far fa-bookmark"></i></button>
+      <button className="button round" onClick={props.addFavorite}><i className="far fa-heart"></i></button>
+      <button className="button round" onClick={props.turnPageBack}><i className="fas fa-angle-left"></i></button>
+      <button className="button round" onClick={props.turnPageForward}><i className="fas fa-angle-right"></i></button>
     </div>
   )
 }
