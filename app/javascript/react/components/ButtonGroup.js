063399e96f578b6e82dfd37a42f0be6f34d89526
James Hartman <hartman.james.p@gmail.com>
2018-02-15 21:17:50 -0500

    autohide buttons on fullscreen, hide on mobile.

--- app/javascript/react/components/ButtonGroup.js
+++ app/javascript/react/components/ButtonGroup.js
@@ -2,21 +2,16 @@ import React from 'react';
 
 
 const ButtonGroup = (props) => {
-  let visibility
-  if (props.navVisible){
-    visibility = {visibility: 'visible'}
-  } else{
-    visibility = {visibility: 'hidden'}
-  }
-
   return(
-    <div className={`comic-nav`} id={props.id}>
-      <button className="button round" onClick={props.goToBegining}><i className="fas fa-angle-double-left"></i></button>
-      <button className="button round" onClick={props.goFull}><i className="fas fa-expand"></i></button>
-      <button className="button round" onClick={props.bookmarkPage}><i className="far fa-bookmark"></i></button>
-      <button className="button round" onClick={props.addFavorite}><i className="far fa-heart"></i></button>
-      <button className="button round" onClick={props.turnPageBack}><i className="fas fa-angle-left"></i></button>
-      <button className="button round" onClick={props.turnPageForward}><i className="fas fa-angle-right"></i></button>
+    <div className="hideable" >
+      <div className={`comic-nav`} id={props.id}>
+        <button className="button round" onClick={props.goToBegining}><i className="fas fa-angle-double-left"></i></button>
+        <button className="button round" onClick={props.goFull}><i className="fas fa-expand"></i></button>
+        <button className="button round" onClick={props.bookmarkPage}><i className="far fa-bookmark"></i></button>
+        <button className="button round" onClick={props.addFavorite}><i className="far fa-heart"></i></button>
+        <button className="button round" onClick={props.turnPageBack}><i className="fas fa-angle-left"></i></button>
+        <button className="button round" onClick={props.turnPageForward}><i className="fas fa-angle-right"></i></button>
+      </div>
     </div>
   )
 }
