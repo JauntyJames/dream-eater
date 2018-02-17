92a8e4e525efabb6a6cef1745edb7f3dd1e5c80c
James Hartman <hartman.james.p@gmail.com>
2018-02-17 15:58:01 -0500

    initial config for cloudinary

--- app/uploaders/comic_file_uploader.rb
+++ app/uploaders/comic_file_uploader.rb
@@ -2,8 +2,9 @@ require 'carrierwave/processing/rmagick'
 
 class ComicFileUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include Cloudinary::Carrierwave
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.test?
@@ -16,6 +17,26 @@ class ComicFileUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+
+  version :standard do
+    process tags: ['comic_file']
+  end
+
+#   version :rotated do
+#   eager
+#   cloudinary_transformation :transformation => [
+#       {:width => 150, :height => 200, :crop => :fill, :effect => "sepia"},
+#       {:angle => 10}
+#     ]
+# end
+
+  version :thumb do
+    process tags: ['comic_thumb']
+    cloudinary_transformation transformation: [
+      { page: 1, convert: 'jpg', resize_to_fit: [300, 300] }
+    ]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -34,21 +55,21 @@ class ComicFileUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  def cover
-    manipulate! do |frame, index|
-      frame if index.zero? # take only the first page of the file
-    end
-  end
-
-  version :thumb do
-    process :cover
-    process :resize_to_fit => [400, 400]
-    process :convert => :jpg
-
-    def full_filename (for_file = model.source.file)
-      super.chomp(File.extname(super)) + '.jpg'
-    end
-  end
+  # def cover
+  #   manipulate! do |frame, index|
+  #     frame if index.zero? # take only the first page of the file
+  #   end
+  # end
+  #
+  # version :thumb do
+  #   process :cover
+  #   process :resize_to_fit => [400, 400]
+  #   process :convert => :jpg
+  #
+  #   def full_filename (for_file = model.source.file)
+  #     super.chomp(File.extname(super)) + '.jpg'
+  #   end
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
