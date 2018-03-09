4551c21a391bd30faa73c89c85047d5657925340
James Hartman <hartman.james.p@gmail.com>
2018-03-09 10:37:20 -0500

    tested from_omniauth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,23 +18,41 @@ RSpec.describe User, type: :model do
     end
   end
 
+  user_auth = Auth.new({email: "user@domain.com", image: "http://www.address.com"}, "facebook", 456234)
+  let!(:user1) { User.from_omniauth( user_auth ) }
+  let!(:user2) { FactoryBot.create(:user, role: "member") }
+  let!(:user3) { FactoryBot.create(:user, role: "admin") }
+
   describe "#admin?" do
     it "is not an admin if the role is not admin" do
-      user = FactoryBot.create(:user, role: "member")
-      expect(user.admin?).to eq(false)
+      expect(user2.admin?).to eq(false)
     end
 
     it "is an admin if the role is admin" do
-      user = FactoryBot.create(:user, role: "admin")
-      expect(user.admin?).to eq(true)
+      expect(user3.admin?).to eq(true)
+    end
+  end
+
+  describe "#apply_omniauth" do
+    it "updates a user with omniauth data" do
+      user3.apply_omniauth(user_auth)
+      expect(user3.provider).to eq "facebook"
     end
   end
 
   describe "#from_omniauth" do
     it "associates a user with an omniauth account" do
-      user_auth = Auth.new({email: "user@domain.com", image: "http://www.address.com"}, "facebook", 456234)
-      user1 = User.from_omniauth( user_auth )
       expect(user1.provider).to eq "facebook"
+    end
+  end
+
+  describe "#has_facebook_linked?" do
+    it "returns true if created from omniauth" do
+      expect(user1.has_facebook_linked?).to eq true
+    end
+    
+    it "returns false if created not from omniauth" do
+      expect(user2.has_facebook_linked?).to eq false
     end
   end
 end
